3ec2d8aa1afdceb62c898e8546219f87df2d7795
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2022-09-11 21:13:28 +0300

    Lab_1_halfVersion
    Tasks:
       +First
       +Second
       +Third(optional)
    
    Input:
       +console
       +random
    
    ArrayMode:
       +static

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,211 +1,547 @@
-﻿ // ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <Windows.h>
 #include <iostream>
 #include <fstream>
 #include <ios>
 #include <vector>
 
-#include <time.h>
 
-using namespace std;
+void CText(const char t[], int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    std::cout << t;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+}
 
-typedef double* pDouble;
-/*
-*   ConsoleInputArrayDouble
-*   
-*/
+int userSize;
+const int ARRAY_MAX_SIZE = 560;
+
+enum { AStatic, ANew, ACalloc, AVec };
+int AMode;
+
 int ConsoleInputSizeArray(const int sizeMax)
 {
     int size = 0; 
+    char buff[256];
+
+    char buf[16];
+    _itoa_s(sizeMax, buf, 10);
+
     do {
-        cout << " Input size Array ( 0< 1 < " << sizeMax << " ) ";
-        cin >> size;
+        CText("  Введіть розмір масиву ", 3);
+        CText("[1 - ", 12);
+        CText(buf, 12);
+        CText("]", 12);
+        CText(" : ", 3);
+        std::cin >> size;
+
+        if (std::cin.fail()) {
+            CText(" Невірно введений розмір!   \n", 112);
+           
+            std::cin.clear();
+            std::cin >> buff;
+        }
     } while (size <= 0 || size >= sizeMax);
     return size;
 }
-/*
-*   ConsoleInputArrayDouble
-*
-*/
-int ConsoleInputArray(int sizeMax, double A[])
-{
+
+int ConsoleInputArray(int sizeMax, double A[]) {
+    char buff[256];
+    char buf[16];
+    
     int size = ConsoleInputSizeArray(sizeMax);
-        for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> A[i];
+    for (int i = 0; i < size; i++) {
+        _itoa_s(i, buf, 10);
+        CText("  Введіть елемент ", 3);
+        CText("[", 12);
+        CText(buf, 12);
+        CText("] ", 12);
+        CText(": ", 3);
+
+        std::cin >> A[i];
+        if (std::cin.fail()) {
+            CText(" Неправильне значення! Введіть раціональні число!   \n", 112);
+
+            std::cin.clear();
+            std::cin >> buff;
+            i--;
+        }
+
     }
     return size;
 }
 
-/*
-*   RndInputArrayDouble
-*
-*/
+
 int RndInputArray(int sizeMax, double A[])
 {
     int size = ConsoleInputSizeArray(sizeMax);
-    int r1=0, r2=0;
-    srand(size);
+    int r1 = 0, r2 = 0, r3 = 0;
+    srand(time(NULL));
 
+    std::cout << "  ";
     for (int i = 0; i < size; i++) {
         r1 = rand();
         r2 = rand();
+        r3 = rand();
         A[i] = 100.0 * r1;
         A[i] = A[i] / (1.0 + r2);
-        cout << A[i] << "   ";
+        if (r3 % 3 == 0) A[i] = -A[i];
+        std::cout << A[i] << "   ";
     }
     return size;
 }
 
-int ConsoleInputDynamicArrayNew(int sizeMax, pDouble &pA)
-{
+int ConsoleInputDynamicArrayNew(int sizeMax, double* pA) {
+    char buff[256];
+    char buf[16];
+
     int size = ConsoleInputSizeArray(sizeMax);
     pA = new double[size];
     if (pA == nullptr) { return 0; }
     for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> pA[i];
+        _itoa_s(i, buf, 10);
+        CText("  Введіть елемент ", 3);
+        CText("[", 12);
+        CText(buf, 12);
+        CText("] ", 12);
+        CText(": ", 3);
+
+        std::cin >> pA[i];
+        if (std::cin.fail()) {
+            CText(" Неправильне значення! Введіть раціональні число!   \n", 112);
+
+            std::cin.clear();
+            std::cin >> buff;
+            i--;
+        }
+
     }
     return size;
 }
 
-int ConsoleInputDynamicArray_calloc(int sizeMax, pDouble& pA)
-{
+int ConsoleInputDynamicArray_calloc(int sizeMax, double* pA) {
+    char buff[256];
+    char buf[16];
+
     int size = ConsoleInputSizeArray(sizeMax);
     pA = (double*)calloc(size, sizeof(double));      // pA = (double*)malloc(size * sizeof(double)); 
     if (pA == nullptr) { return 0; }
     for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> pA[i];
+        _itoa_s(i, buf, 10);
+        CText("  Введіть елемент ", 3);
+        CText("[", 12);
+        CText(buf, 12);
+        CText("] ", 12);
+        CText(": ", 3);
+
+        std::cin >> pA[i];
+        if (std::cin.fail()) {
+            CText(" Неправильне значення! Введіть раціональні число!   \n", 112);
+
+            std::cin.clear();
+            std::cin >> buff;
+            i--;
+        }
+
     }
     return size;
 }
 
-void ConsoleInputVector(int sizeMax, vector<double> &A)
-{
+int ConsoleInputVector(int sizeMax, std::vector<double> &A) {
+    char buff[256];
+    char buf[16];
+
     int size = ConsoleInputSizeArray(sizeMax);
     double d;
+
     for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
+        _itoa_s(i, buf, 10);
+        CText("  Введіть елемент ", 3);
+        CText("[", 12);
+        CText(buf, 12);
+        CText("] ", 12);
+        CText(": ", 3);
+
+        std::cin >> d;
+        if (std::cin.fail()) {
+            CText(" Неправильне значення! Введіть раціональні число!   \n", 112);
+
+            std::cin.clear();
+            std::cin >> buff;
+            i--;
+        }
+        else {
+            A.push_back(d);
+        }
+
     }
-    return ;
+    return size;
 }
 
 
-/*
-*  WriteArrayTextFile 
-*
-*/
 
-void WriteArrayTextFile(int n, double *arr, const char *fileName )
-{
-    ofstream fout(fileName);
+void WriteArrayTextFile(int n, double *arr, const char *fileName ) {
+    std::ofstream fout(fileName);
     if (fout.fail()) return;
-    fout << n << endl;
+    fout << n << std::endl;
     for (int i = 0; i < n; i++)
         fout << arr[i] << "   ";
-    fout.close(); //
-}
-/*
-*  ReadArrayTextFile
-*
-*/
 
+    CText("\n\n  ", 0);
+    CText("Дані збережено у файл  ", 112);
+    CText(fileName, 128);
+    CText("\n  ", 0);
+   
+    char full[_MAX_PATH];
+    if (_fullpath(full, fileName, _MAX_PATH) != NULL)
+        CText(full, 128);
 
-int ReadArrayTextFile(int n, double* arr, const char* fileName)
-{
-    int size;
-    ifstream fin(fileName);
-    if (fin.fail()) return 0;
-    fin >> size;
-    if (size <= 0) return 0;
-    if (size > n) size = n;   
-    for (int i = 0; i < n; i++)
-       fin>> arr[i];
-    fin.close();
-    return size;
+    CText("\n", 0);
+
+    fout.close(); 
 }
 
 
-void WriteArrayBinFile(int n, double* arr, const char* fileName)
-{
-    //ios_base
-    ofstream bfout(fileName, ios_base::binary);
-    if (bfout.fail()) return;
-    bfout.write((const char*)&n, sizeof(int));
-    std::streamsize  cn = static_cast<std::streamsize>(n) *sizeof(double);
-    bfout.write((const char*)arr, cn);
-    bfout.close();
+
+void ShowMainMenu();
+void MenuManager();
+
+
+
+void Task1_v11(int arrSize, double *arr) {
+
+    if (arrSize == 1)
+        arr[0] *= 2;
+    else
+        for (int i = 0; i < arrSize / 2; i++)
+            arr[i] *= 2;
+
+    for (int i = (arrSize / 2); i <= arrSize; i++)
+        arr[i] *= 3;
+
+    std::cout << "  ";
+    for (int i = 0; i < arrSize; i++)
+        std::cout << arr[i] << "   ";
+   
 }
 
-int ReadArrayBinFile(int n, double* arr, const char* fileName)
-{
-    int size=0;
-    ifstream bfin(fileName, ios_base::binary);
-    if (bfin.fail()) return 0;
-    bfin.read((char*)&size, sizeof(int));
-    if (size <= 0) return 0;
-    if (size > n) size = n;
-    bfin.read((char*)arr, static_cast<std::streamsize>(size) * sizeof(double));
-    bfin.close();
-    return size;
-}
 
-void ShowMainMenu()
-{
-    cout << "    Main Menu  \n";
-    cout << "    1.  Task 1  \n";
-    cout << "    2.  Task 2  \n";
-    cout << "    3.  Task 3  \n";
-    cout << "    e.  Task 7  \n";
-    cout << "    t.  Task 5  \n";
-    cout << "    5.  Task 5  \n";
 
-}
+void Task2_v11(int arrSize, double* arr) {
+    int pos1 = 0;
+    int pos2 = 0;
+    int size = 0;
+    double elMin = 0;
+    char buf[16];
+    bool fl = 0;
 
-void MenuTask()
-{
-    cout << "     Menu Task 1  \n";
-    cout << "    1.  Local array  \n";
-    cout << "    2.  Dynamic array 1 \n";
-    cout << "    3.  Dynamic array 2 \n";  
-    ///    
-/// rr
-}
-int main()
-{
-    const int MAX_SIZE = 560;
-    std::cout << "Hello World!\n";
-    ShowMainMenu();
-    double A[MAX_SIZE], B[MAX_SIZE],C[MAX_SIZE];
-    int n,m;
-    n = RndInputArray(MAX_SIZE, A);
-    WriteArrayTextFile(n, A, "1.txt");
-    m = ReadArrayTextFile(MAX_SIZE, B, "1.txt");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << B[i] << "   ";
-    WriteArrayBinFile(n, A, "1.bin");
-    m = ReadArrayBinFile(MAX_SIZE, C, "1.bin");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << C[i] << "   ";
-    cout << "\n  Vector \n";
-    vector<double> vA;
-    ConsoleInputVector(MAX_SIZE, vA);
-    for (auto v : vA) {
-        cout << v << "   ";
+    while (arr[pos1] >= 0) {     // Finding first negative element
+        pos1++;
+        if (pos1 > arrSize - 1) {
+            std::cout << " ";
+            CText("У масиві не має від'ємних елементів! Введіть масив знову.\n", 112);
+            fl = 1;
+            break;
+        }
+    }
+    if (fl) return;
+
+
+    pos1++;
+    size = arrSize - pos1;
+    elMin = (double)pos1;
+
+    if (pos1 < arrSize - 1) {       // Summing by pare, and moving everything
+        while (pos1 < arrSize) {
+            arr[pos1] += arr[pos1 + 1];
+            pos2 = pos1 + 1;
+            while (pos2 < arrSize) {
+                arr[pos2] = arr[pos2 + 1];
+                if (pos2 == arrSize - 1) arr[pos2] = 0;
+                pos2++;
+
+            }
+            arrSize--;
+            pos1++;
+        }
+    }
+    if (pos1 != arrSize - 1)    // Exception wrong size
+        if (size % 2 != 0) arrSize++;
+
+
+
+    pos1 = elMin;
+    pos2 = pos1;
+    elMin = arr[pos1]; // Minimal element
+
+    
+
+    while (pos1 < arrSize) {        // Finding last minimal element from pos1
+        if (arr[pos1] < elMin) {
+            elMin = arr[pos1];
+            pos2 = pos1;
+        }
+        pos1++;
     }
 
-    return 1;
+    std::cout << "  ";
+    for (int i = 0; i < arrSize; i++)
+        std::cout << arr[i] << "   ";
+    std::cout << "\n";
+
+    if (pos2 != arrSize) {
+        _itoa_s(pos2 + 1, buf, 10);
+        std::cout << "  ";
+        CText("Номер останього мінімального елемента за умовою : ", 112);
+        CText(" ", 128);
+        CText(buf, 128);
+        CText(" ", 128);
+        std::cout << "\n";
+    }
+    else {
+        std::cout << "  ";
+        CText("Не існує мінімального елементу за умовою задачі!\n", 112);
+    }
+   
+   
+}
+
+
+
+
+float area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
+}
+
+bool isInside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y) {
+ 
+    float A = area(x1, y1, x2, y2, x3, y3);
+    float A1 = area(x, y, x2, y2, x3, y3);
+    float A2 = area(x1, y1, x, y, x3, y3);
+    float A3 = area(x1, y1, x2, y2, x, y);
+
+    return (A == A1 + A2 + A3);
+}
+
+
+void Task3_v11(int arrSize, double* arr, double* arr2) {
+
+    int cnt = 0;
+    char buff[16];
+
+    std::cout << "  ";
+    CText("Заданий трикутник: ", 112);
+    for (int i = 0; i < 3; i++) {
+        CText("(", 128); _itoa_s(arr2[i], buff, 10); CText(buff, 128);
+        CText(", ", 128); _itoa_s(arr2[i + 1], buff, 10); CText(buff, 128);
+        CText(")", 128); CText("   ", 112);
+    }
+    std::cout << '\n';
+
+    for (int i = 0; i < userSize; i += 2) {   // Calculating here
+       
+        std::cout << "  ";
+        CText("Точка ", 112);
+        CText("(", 128); _itoa_s(arr[i], buff, 10); CText(buff, 128);
+        CText(", ", 128); _itoa_s(arr[i + 1], buff, 10); CText(buff, 128);
+        CText(")", 128); CText("   ", 112);
+      
+        if (isInside(arr2[0], arr2[1], arr2[2], arr2[3], arr2[4], arr2[5], arr[i], arr[i + 1]))
+            CText(" належить трикутнику", 115);
+        else
+            CText(" не належить трикутнику", 116);
+        std::cout << '\n';
+    }
+}
+
+
+
+void ShowInputMenu() {
+
+    std::cout << "\n  ";
+    CText(" Виберіть спосіб введення данних   \n", 225);
+    std::cout << '\n';
+    CText("  1. Рандомно\n", 11);
+    CText("  2. З консолі (статичний масив)\n", 11);
+   /* CText("  3. З консолі (calloc)\n", 11);
+    CText("  4. З консолі (new)\n", 11);
+    CText("  5. З консолі (vector)\n", 11);*/
+    CText("  3. Вийти \n\n\n", 11);
+
+    CText("  Виберіть спосіб з меню : ", 3);
 
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+int InputManager(double* arr, double* arr2, double* arrNew, double* arrCalloc, std::vector<double> arrVec) {
+    int key;
+    char buff[256];
+    char fileAddress[1024];
+    memset(fileAddress, 0, sizeof(fileAddress));
+
+    std::cin >> key;
+
+    if (std::cin.fail()) {
+        std::cout << "  ";
+        CText("Невірно введений запит! Введіть число від 1 до 3\n", 112);
+        std::cin.clear();
+        std::cin >> buff;
+        InputManager(arr, arr2, arrNew, arrCalloc, arrVec);
+    }
+    else switch (key) {
+
+    case 1:
+        userSize = RndInputArray(ARRAY_MAX_SIZE, arr);
+        break;
+
+    case 2:
+        AMode = AStatic;
+        userSize = ConsoleInputArray(ARRAY_MAX_SIZE, arr);
+        break;
+
+    /*case 3:
+        AMode = ACalloc;
+        userSize = ConsoleInputDynamicArray_calloc(ARRAY_MAX_SIZE, arrCalloc);
+        break;
+
+    case 4:
+        AMode = ANew;
+        userSize = ConsoleInputDynamicArrayNew(ARRAY_MAX_SIZE, arrNew);
+        break;
+
+    case 5:
+        AMode = AVec;
+        userSize = ConsoleInputVector(ARRAY_MAX_SIZE, arrVec);
+        break;*/
+  
+       
+    case 3:
+        break;
+
+    default:
+        std::cout << "  ";
+        CText("Вибраний не правильний спосіб!\n", 112);
+        CText("\n\n\n  Виберіть спосіб з меню : ", 3);
+        InputManager(arr, arr2, arrNew, arrCalloc, arrVec);
+        break;
+    }
+
+    return 0;
+}
+
+
+void ShowMainMenu() {
+
+    std::cout << "  ";
+    CText("         Меню          \n", 225);
+    std::cout << '\n';
+    CText("  1. N_1_V11 Помножити першу половину масиву на 2, другу на 3   \n", 11);
+    CText("  2. N_2_V11 Знайти номер мінімального ел. серед попарно доданих ел., що правіше, за перший від'ємний ел.\n", 11);
+    CText("  3. N_3_V11 Задати трикутник. Задати довільні точки. Визначити, скільки точок належать трикутнику\n", 11);
+    CText("  4. Показати меню (Очистити вікно)\n", 11);
+    CText("  5. Вийти \n\n\n", 11);
+
+    CText("  Виберіть дію з меню : ", 3);
+}
+
+void MenuManager(double *arr, double *arr2, double *arrNew, double *arrCalloc, std::vector<double> arrVec) {
+    int key;
+    char buff[256];
+
+    std::cin >> key;
+
+    if (std::cin.fail()) {
+        std::cout << "  ";
+        CText("Невірно введений запит! Введіть число від 1 до 5\n", 112);
+        std::cin.clear();
+        std::cin >> buff;
+        MenuManager(arr, arr2, arrNew, arrCalloc, arrVec);
+    }
+    else switch (key) {
+
+    case 1:
+        ShowInputMenu();
+        InputManager(arr, arr2, arrNew, arrCalloc, arrVec);
+        if (AMode == AStatic) WriteArrayTextFile(userSize, arr, "Task_1_Original_Array.txt");
+        //if (AMode == ANew) WriteArrayTextFile(userSize, arrNew, "Task_1_Original_Array.txt");
+        //if (AMode == ACalloc) WriteArrayTextFile(userSize, arrCalloc, "Task_1_Original_Array.txt");
+
+        CText("\n\n  Результат задачі : \n", 3);
+        if (AMode == AStatic) Task1_v11(userSize, arr);
+        //if (AMode == ANew) Task1_v11(userSize, arr);
+        //if (AMode == ACalloc) Task1_v11(userSize, arr);
+        
+        if (AMode == AStatic) WriteArrayTextFile(userSize, arr, "Task_1_Modificated_Array.txt");
+        //if (AMode == ANew) WriteArrayTextFile(userSize, arrNew, "Task_1_Modificated_Array.txt");
+        //if (AMode == ACalloc) WriteArrayTextFile(userSize, arrCalloc, "Task_1_Modificated_Array.txt");
+        CText("\n\n\n  Виберіть дію з меню : ", 3);
+        MenuManager(arr, arr2, arrNew, arrCalloc, arrVec);
+
+    case 2:
+        ShowInputMenu();
+        InputManager(arr, arr2, arrNew, arrCalloc, arrVec);
+        if (AMode == AStatic) WriteArrayTextFile(userSize, arr, "Task_2_Original_Array.txt");
+        //if (AMode == ANew) WriteArrayTextFile(userSize, arrNew, "Task_2_Original_Array.txt");
+        //if (AMode == ACalloc) WriteArrayTextFile(userSize, arrCalloc, "Task_2_Original_Array.txt");
+
+        CText("\n\n  Результат задачі : \n", 3);
+        if (AMode == AStatic) Task2_v11(userSize, arr);
+        //if (AMode == ANew) Task2_v11(userSize, arrNew);
+        //if (AMode == ACalloc) Task2_v11(userSize, arrCalloc);
+
+        if (AMode == AStatic) WriteArrayTextFile(userSize, arr, "Task_2_Modificated_Array.txt");
+        //if (AMode == ANew) WriteArrayTextFile(userSize, arrNew, "Task_2_Modificated_Array.txt");
+        //if (AMode == ACalloc) WriteArrayTextFile(userSize, arrCalloc, "Task_2_Modificated_Array.txt");
+
+        CText("\n\n\n  Виберіть дію з меню : ", 3);
+        MenuManager(arr, arr2, arrNew, arrCalloc, arrVec);
+
+    case 3:
+        CText("  Введіть 6 координат трикутника (x1, y1)..\n", 3);
+        ConsoleInputArray(7, arr2);
+        WriteArrayTextFile(userSize, arr2, "Task_3_Points_Array.txt");
+
+        CText("  Введіть координати точок (x1, y1)..\n", 3);
+        userSize = ConsoleInputArray(ARRAY_MAX_SIZE, arr);
+       
+        CText("\n\n  Результат задачі : \n", 3);
+        Task3_v11(userSize, arr, arr2);
+        WriteArrayTextFile(userSize, arr2, "Task_3_Triangle_Array.txt");
+        CText("\n\n\n  Виберіть дію з меню : ", 3);
+        MenuManager(arr, arr2, arrNew, arrCalloc, arrVec);
+
+    case 4:
+        system("cls");
+        ShowMainMenu();
+        MenuManager(arr, arr2, arrNew, arrCalloc, arrVec);
+
+    case 5:
+        exit(0);
+
+    default:
+        std::cout << "  ";
+        CText("Вибране не правильне завдання!\n", 112);
+        CText("\n\n\n  Виберіть дію з меню : ", 3);
+        MenuManager(arr, arr2, arrNew, arrCalloc, arrVec);
+        break;
+    }
+    
+}
+
+
+
+int main() {
+
+    system("chcp 1251");
+    system("cls");
+
+    double A[ARRAY_MAX_SIZE], B[ARRAY_MAX_SIZE],C[ARRAY_MAX_SIZE];
+
+    double* ANew = NULL;
+    double* ACalloc = NULL;
+    std::vector<double> AVector;
+
+    ShowMainMenu();
+    MenuManager(A, B, ANew, ACalloc, AVector);
+    
+    return 0;
+}
